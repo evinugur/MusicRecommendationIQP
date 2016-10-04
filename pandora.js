6d04f2fdf445f4e6dd3c59ee72142bac9a66af07
Evin Ugur <etugur@wpi.edu>
2016-10-04 17:39:23 -0400

    we can successfully intercept ui events on pandora

--- pandora.js
+++ pandora.js
@@ -6,21 +6,30 @@ var clickableClassNames = ['thumbDownButton', 'thumbUpButton', 'playButton', 'pa
 
 var TIMEOUT_INTERVAL = 500;
 
+var loadingTime = 0;
+
 function bindEventsAfterSplashScreen() {
 	if (document.getElementById("splash").style.display !== 'none') {
 		// the splash screen is still visible; try again
-		console.log("still loading");
+		console.log(++loadingTime + ".) Loading Splash Screen");
 		window.setTimeout(bindEventsAfterSplashScreen, TIMEOUT_INTERVAL);
 	}
 	else {
 		// additional timeout delay just to wait and make sure
 		setTimeout(function() {
-			clickableClassNames.forEach(function(currentValue, index, array) {
-				console.log(currentValue);
-			});
+			injectListeners();			
 		}, TIMEOUT_INTERVAL);
 	}
 }
 
 
+function injectListeners() {
+	clickableClassNames.forEach(function(currentValue, index, array) {
+			var element = document.getElementsByClassName(currentValue)[0].children[0];
+			element.onclick = function() {console.log(currentValue + " was clicked");};
+	});
+}
+
 bindEventsAfterSplashScreen();
+
+
