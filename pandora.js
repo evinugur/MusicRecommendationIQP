832c72cabf11d3cfcf50d6f67a95045d245ad32b
Evin Ugur <etugur@wpi.edu>
2016-10-05 18:18:27 -0400

    tracking refactor for station details page

--- pandora.js
+++ pandora.js
@@ -1,16 +1,30 @@
+// keys can be used on a dictionary to override default tracking methods when calling the track
+var KEY_EVENT = "event";
+var KEY_STATION_NAME = "station";
+
+// hardcoded values for KEY_EVENT
+var EVENT_THUMBS_DOWN = "Tumbs Down";
+var EVENT_THUMBS_UP = "Thumbs Up";
+var EVENT_PLAY = "Play";
+var EVENT_PAUSE = "Pause";
+var EVENT_SKIP = "Skip";
+
 // these refer to events that can be tracked directky by clicking on a DOM element
 var clickableClassNames = [
-	{className: 'thumbDownButton', eventName: "Thumbs Down"},
-	{className: 'thumbUpButton', eventName: "Thumbs Up"},
-	{className: 'playButton', eventName: "Play"},
-	{className: 'pauseButton', eventName: "Pause"},
-	{className: 'skipButton', eventName: "Skip"}];
+	{className: 'thumbDownButton', eventName: EVENT_THUMBS_DOWN},
+	{className: 'thumbUpButton', eventName: EVENT_THUMBS_UP},
+	{className: 'playButton', eventName: EVENT_PLAY},
+	{className: 'pauseButton', eventName: EVENT_PAUSE},
+	{className: 'skipButton', eventName: EVENT_SKIP}];
+
 
 /* chrome will run the script when the page is loading; this gets tricky becuase Pandora loads a splashscreen with a 
 multitutde of async requests. What we can do is periorically probe until the splash screen is gone in the dom via timeout
 polling */
 
 var TIMEOUT_INTERVAL = 500;
+
+
 
 // just used for diagnostic printing; shows how the current async timeout request we are on
 var loadingDelayCount = 0;
@@ -37,16 +51,19 @@ function injectListeners() {
 	clickableClassNames.forEach(function(currentValue, index, array) {
 			var element = document.getElementsByClassName(currentValue.className)[0].children[0];
 			element.addEventListener("click", function() {
-				// TODO make logging event request here - we can look at currentValue to look at what was played
-				console.log("Current User:\t" + getCurrentUsername());
-				console.log("Current Station ID:\t" + getCurrentStationId());
-				console.log("Current Station Name:\t" + getCurrentStationName());
-				console.log("TRACK EVENT:\t" + currentValue.eventName);
+				track({KEY_EVENT: currentValue.eventName});
 			});
 	});
 }
 
-bindEventsAfterSplashScreen();
+
+function track(data) {
+	data = data || {};
+	console.log("Username:\t" + getCurrentUsername());
+	console.log("Station ID:\t" + getCurrentStationId());
+	console.log("Station Name:\t" + (data.KEY_STATION_NAME || getCurrentStationName()));
+	console.log("Event:\t" + (data.KEY_EVENT || "ERROR"));
+}
 
 function getCurrentStationId() {
 	// ie https://www.pandora.com/station/3333039448775710377/fans
@@ -76,4 +93,12 @@ If we can't get the station ID then it is possible to just rely on station name.
 the DOM such that users can't rename a station. Plus if they have multiple stations that's bad.
 
 Also need to code a way for a user to track when a user adds a seed to a station, or possibly removes one as well.
+
+Should track song when click occurs
+
 */
+
+
+
+
+bindEventsAfterSplashScreen();
