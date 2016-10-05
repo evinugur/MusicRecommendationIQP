6c735b4065c005c6fbfcca2ace9da5dc1b25ab99
Evin Ugur <etugur96@gmail.com>
2016-10-05 16:29:41 -0400

    comments with notes on recent developments and how they'll be implemented in software

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ script.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head || document.documentElement).appendChild(script);
+
+/**
+The manifest gives us full access to run app.js (and any of the web-accessible resources defiend in the manifest such as pandora.js) anywhere 
+on the Pandora domain. 
+
+In order to track events that aren't on the main screen, such as when a user removes a seed from a station, we'd possibly want to analyze
+the URL scheme in this file and conditionally inject different content scripts and/or injecting multiple scripts
+*/
--- pandora.js
+++ pandora.js
@@ -1,3 +1,4 @@
+// these refer to events that can be tracked
 var clickableClassNames = [
 	{className: 'thumbDownButton', eventName: "Thumbs Down"},
 	{className: 'thumbUpButton', eventName: "Thumbs Up"},
@@ -12,12 +13,11 @@ polling */
 var TIMEOUT_INTERVAL = 500;
 
 // just used for diagnostic printing; shows how the current async timeout request we are on
-var loadingTime = 0;
-
+var loadingDelayCount = 0;
 function bindEventsAfterSplashScreen() {
 	if (document.getElementById("splash").style.display !== 'none') {
 		// the splash screen is still visible; try again
-		console.log(++loadingTime + ".) Loading Splash Screen");
+		console.log(++loadingDelayCount + ".) Loading Splash Screen");
 		setTimeout(bindEventsAfterSplashScreen, TIMEOUT_INTERVAL);
 	}
 	else {
@@ -30,12 +30,12 @@ function bindEventsAfterSplashScreen() {
 function injectListeners() {
 	clickableClassNames.forEach(function(currentValue, index, array) {
 			var element = document.getElementsByClassName(currentValue.className)[0].children[0];
-			element.onclick = function() {
+			element.addEventListener("click", function() {
 				// TODO make logging event request here - we can look at currentValue to look at what was played
 				console.log("Current User:\t" + getCurrentUsername());
 				console.log("Current Station:\t" + getCurrentStationName());
 				console.log("TRACK EVENT:\t" + currentValue.eventName);
-			};
+			});
 	});
 }
 
@@ -49,3 +49,14 @@ function getCurrentStationName() {
 function getCurrentUsername() {
 	return document.getElementsByClassName('userName')[0].innerHTML;
 }
+
+/* points of concern 
+Need to track when participants add a seed 
+For this we need a notion of station primary key. Each station has a UUID but i'm unsure if that's accessible from within the DOM. This can likely be found
+by looking for links that have the station id as an HTTP param. 
+
+If we can't get the station ID then it is possible to just rely on station name. Although this would be complicated & would likely require modifying
+the DOM such that users can't rename a station. Plus if they have multiple stations that's bad.
+
+Also need to code a way for a user to track when a user adds a seed to a station, or possibly removes one as well.
+*/
