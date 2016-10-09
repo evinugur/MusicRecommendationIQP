6c02d17ead94ac9ebabd14904db6a7b949d38c1a
Evin Ugur <etugur@wpi.edu>
2016-10-09 18:55:46 -0400

    Thumbs up deleted tracking
    Just needs to be generalized for thumbs down

--- pandora.js
+++ pandora.js
@@ -1,18 +1,23 @@
 // keys can be used on a dictionary to override default tracking methods when calling the track
 var KEY_EVENT = "event";
-var KEY_STATION_NAME = "station";
+var KEY_STATION_ID = "station_id";
+var KEY_STATION_NAME = "station_name";
+var KEY_SONG = "song";
+
 
 // hardcoded values for KEY_EVENT
-var EVENT_THUMBS_DOWN = "Tumbs Down";
-var EVENT_THUMBS_UP = "Thumbs Up";
+var EVENT_THUMBS_DOWN_ADDED = "Tumb Down Added";
+var EVENT_THUMBS_UP_ADDED = "Thumb Up Added";
+var EVENT_THUMBS_DOWN_DELETED = "Tumb Down Deleted";
+var EVENT_THUMBS_UP_DELETED = "Thumb Up Deleted";
 var EVENT_PLAY = "Play";
 var EVENT_PAUSE = "Pause";
 var EVENT_SKIP = "Skip";
 
 // these refer to events that can be tracked directky by clicking on a DOM element
 var clickableClassNames = [
-	{className: 'thumbDownButton', eventName: EVENT_THUMBS_DOWN},
-	{className: 'thumbUpButton', eventName: EVENT_THUMBS_UP},
+	{className: 'thumbDownButton', eventName: EVENT_THUMBS_DOWN_ADDED},
+	{className: 'thumbUpButton', eventName: EVENT_THUMBS_UP_ADDED},
 	{className: 'playButton', eventName: EVENT_PLAY},
 	{className: 'pauseButton', eventName: EVENT_PAUSE},
 	{className: 'skipButton', eventName: EVENT_SKIP}];
@@ -54,14 +59,41 @@ function injectListeners() {
 				track({KEY_EVENT: currentValue.eventName});
 			});
 	});
+	if (document.location.href.indexOf("pandora.com/station/") !== -1) {
+		injectStationDetailListeners();
+	}
 }
 
+function injectStationDetailListeners() {
+	var url = document.location.href.split('/');
+	// we need station id from URL since you can view a station's details while playing another station
+	var stationId = Number(url[url.length -1]);
+	var stationName = $('.hed-1')[0].innerHTML.trim();
+	$('.thumb_up_list').find('.deletable').each(function() {
+		var el = this;
+		el.addEventListener("click", function() {
+			var sognContainer = $(el.parentElement.parentElement).find(".col1 a")[0];
+			track({
+				KEY_EVENT: EVENT_THUMBS_UP_DELETED,
+				KEY_STATION_ID: stationId,
+				KEY_STATION_NAME: stationName,
+				KEY_SONG: {name: sognContainer.innerHTML, href: sognContainer.href}
+			});
+		});
+	});
+
+
+
+	$('.thumb_down_list').find('.deletable').addEventListener("click", function() {
+
+	});
+}
 
 function track(data) {
 	data = data || {};
 	console.log("Username:\t" + getCurrentUsername());
-	console.log("Station ID:\t" + getCurrentStationId());
-	console.log("Song Info:\t" + JSON.stringify(getSongInfo()));
+	console.log("Station ID:\t" + (data.KEY_STATION_ID || getCurrentStationId()));
+	console.log("Song Info:\t" + JSON.stringify((data.KEY_SONG || getSongInfo())));
 	console.log("Station Name:\t" + (data.KEY_STATION_NAME || getCurrentStationName()));
 	console.log("Event:\t" + (data.KEY_EVENT || "ERROR"));
 }
@@ -85,6 +117,7 @@ function getCurrentUsername() {
 	return document.getElementsByClassName('userName')[0].innerHTML;
 }
 
+
 function getSongInfo() {
 	var song = $('.songTitle')[0];
 	return {
@@ -100,16 +133,10 @@ Need to track when participants add a seed
 For this we need a notion of station primary key. Each station has a UUID but i'm unsure if that's accessible from within the DOM. This can likely be found
 by looking for links that have the station id as an HTTP param. 
 
-If we can't get the station ID then it is possible to just rely on station name. Although this would be complicated & would likely require modifying
-the DOM such that users can't rename a station. Plus if they have multiple stations that's bad.
-
 Also need to code a way for a user to track when a user adds a seed to a station, or possibly removes one as well.
 
 Should track song when click occurs
-
 */
-
-
 
 
 bindEventsAfterSplashScreen();
