e20a11565453f6292b416efa6c841119f33bd589
Evin Ugur <etugur@wpi.edu>
2016-11-01 13:56:27 -0400

    Added Tunein script into app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var script = document.createElement('script');
+var url = document.location.href;
+var src;
+if (url.indexOf("pandora") !== -1) src = "pandora.js";
+else if (url.indexOf("tunein") !== -1) src = "tunein.js";
+if (!src) return;
 script.src = chrome.extension.getURL('pandora.js');
 script.onload = function() {
     this.parentNode.removeChild(this);
