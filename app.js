883eacfb73303847a602e2c9d4acb6d394aeb4d6
Evin Ugur <etugur@wpi.edu>
2016-11-02 14:03:51 -0400

    enacapsulated script injecting logic

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 (function() {
-var script = document.createElement('script');
-var url = document.location.href;
-var src;
-if (url.indexOf("pandora") !== -1) src = "pandora.js";
-else if (url.indexOf("tunein") !== -1) src = "tunein.js";
-if (!src) return;
-script.src = chrome.extension.getURL('pandora.js');
-script.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(script);})();
+	var url = document.location.href;
+	var src;
+	if (url.indexOf("pandora") !== -1) {
+		injectScript("pandora.js");
+	}
+	else if (url.indexOf("tunein") !== -1) {
+		injectScript("tunein.js");
+	}
+})();
+
+
+function injectScript(src) {
+	var script = document.createElement('script');
+	script.src = chrome.extension.getURL(src);
+	script.onload = function() { this.parentNode.removeChild(this); };
+	(document.head || document.documentElement).appendChild(script);
+}
