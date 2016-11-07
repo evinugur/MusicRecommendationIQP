9367e42c4097dcf632f589bcd8ff9195068f3faf
Evin Ugur <etugur@wpi.edu>
2016-11-07 14:13:11 -0500

    local storage working

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
 window.onload = function() {
 		var STORAGE_KEY_TUNEIN = "tuneinEmail";
+		document.getElementById('tuneinGetButton').onclick = function() {
+			chrome.storage.local.get(STORAGE_KEY_TUNEIN, function(result) {
+				console.log(result[STORAGE_KEY_TUNEIN]);
+				debugger;
+			});
+		};
 		document.getElementById("tuneinButton").onclick = function() {
 			var text = document.getElementById("tuneinEmail").value.trim();
 			if (!text) {
 				console.log("Invalid!");
 				return;
 			}
-			chrome.storage.sync.set({STORAGE_KEY_TUNEIN: text}, function() {
+			var payload = {};
+			payload[STORAGE_KEY_TUNEIN] = text;
+			chrome.storage.local.set(payload, function() {
 				console.log("Saving", text);
 			});
 		};
