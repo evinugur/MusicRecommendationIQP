90eba19fa60fed26172908203d4f5441e2511fc5
Evin Ugur <etugur@wpi.edu>
2016-11-08 16:20:34 -0500

    popup for discovery only enabled on tunein/pandora

--- popup.js
+++ popup.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', init);
 function init() {
 	var settingsBtn = document.querySelector("#settingsBtn");
+	var discoveryBtn = document.querySelector('#discoveryBtn');
 	settingsBtn.addEventListener("click", function() {
 		chrome.tabs.create({'url': chrome.extension.getURL('background.html')});
+	});
+
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var tab = tabs[0];
+		if (tab.url.indexOf("tunein.com") === -1 && tab.url.indexOf("pandora.com") === -1) {
+			discoveryBtn.disabled = true;
+			return;
+		}
+		discoveryBtn.disabled = false;
+	});
+
+
+
+	discoveryBtn.addEventListener('click', function() {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  		chrome.tabs.sendMessage(tabs[0].id, {event: "discovery"}, function(response) {
+	    	console.log(response.farewell);
+  		});
+		});
 	});
 };
