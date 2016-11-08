d4b2a155cdd6ce410d0d08c04b16bd0447cc19e6
Evin Ugur <etugur@wpi.edu>
2016-11-08 18:29:24 -0500

    Fix Single Page App Related Bug via url polling

--- tunein.js
+++ tunein.js
@@ -1,11 +1,24 @@
 window.onload = function() {
-	var url = document.location.href;
-	if (url.indexOf("/radio") === -1) return;
 	var counter = 1;
+	var nextTimeout = null;
+	var URL_POLLING_INTERVAL = 100;
+	var URL_CACHE = null;
+	var URL_CHANGE_CALLBACK = null;
 	var track = function() {
+		var url = document.location.href;
+		if (url.indexOf("/radio") === -1) return;
 		window.postMessage({ type: "FROM_PAGE", text: counter + ' ' + url }, "*");
 		counter++;
-		setTimeout(track, 1000 * 60 * 5);
+		nextTimeout = setTimeout(track, 1000 * 60 * 5);
 	};
-	track();	
+	var urlPolling = function() {
+		if (URL_CACHE !== document.location.href) {
+			URL_CACHE = document.location.href;
+			counter = 1;
+			if (nextTimeout)  clearTimeout(nextTimeout);
+			track();
+		}
+		setTimeout(urlPolling, URL_POLLING_INTERVAL);
+	}
+	urlPolling();
 };
